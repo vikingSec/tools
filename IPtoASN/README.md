916842c8ece42d0e63deafc7bea1dbb66119fde9
vikingSec <mgedwar1@gmail.com>
2018-03-19 02:55:15 -0500

    ipToASN tool
    Added a tool for converting a list of IPs to an Excel sheet with IPs and ASNs/Locations.

---
+++ IPtoASN/README.md
@@ -0,0 +1,8 @@
+<html>
+<h1>IP to ASN QuickScript </h1>
+
+Quick script I wrote to take a text list of IPs and export them to an EXCEL file with ASN and location data. May be useful for threat hunting stuff. Included a test file with 5 ips that were said to be malicious online.<br><br>
+
+<b>Usage:</b><br>
+python ./ipInfo.py [path to text file]<br>
+</html>
---
+++ IPtoASN/ipInfo.py
@@ -0,0 +1,53 @@
+import requests
+from bs4 import BeautifulSoup
+import geoip2.database
+import xlsxwriter
+
+def main(f='testIPlist.txt'):
+    Data = open(f)
+    ips_full = addInfo(Data)
+    toXlsx(ips_full, './ip_list.xlsx')
+    
+    
+    
+
+
+    return inThrees
+
+def addInfo(data):
+    fullData = [[]]
+    reader_city = geoip2.database.Reader('./City/City.mmdb')
+    reader_country = geoip2.database.Reader('./Country/Country.mmdb')
+    reader_asn = geoip2.database.Reader('./ASN/ASN.mmdb')
+    
+    for line in data:
+        if len(line) > 0:
+
+            line = line.strip()
+            city_response = reader_city.city(line)
+            city = city_response.city.name
+            country_response = reader_country.country(line)
+            country = country_response.country.name
+            try:
+                asn_response = reader_asn.asn(line)
+                asn = str(asn_response.autonomous_system_number)+': '+asn_response.autonomous_system_organization
+            except:
+                asn = 'UNKNOWN'
+            fullData.append([line,country,city,asn])
+    return fullData
+
+def toXlsx(data, filename):
+    
+    row = 0
+    workbook = xlsxwriter.Workbook(filename)
+    worksheet = workbook.add_worksheet()
+    for line in data:
+        col = 0
+        for item in line:
+            worksheet.write(row, col, item)
+            col+=1
+        row+=1
+        
+    
+                           
+main()
