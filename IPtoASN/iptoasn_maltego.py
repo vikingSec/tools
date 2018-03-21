0d942cc293889b3c33b12ad8a6a70bc1a322ea66
Odin <mgedwar1@gmail.com>
2018-03-21 17:53:25 -0500

    Actually learned how to Maltego TRX API

--- IPtoASN/iptoasn_maltego.py
+++ IPtoASN/iptoasn_maltego.py
@@ -4,16 +4,6 @@ import sys
 
 
 val = sys.argv[1]
-print("""<MaltegoMessage>
-    <MaltegoTransformResponseMessage>
-        <Entities>
-            <Entity Type="maltego.AS">
-                <Value>"""+IPtoASN.getASN(val)+"""</Value>
-                <Weight>100</Weight>
-            </Entity>
-        </Entities>
-        <UIMessages>
-        </UIMessages>
-    </MaltegoTransformResponseMessage>
-</MaltegoMessage>
-""")
+trx = MaltegoTransform()
+trx.addEntity('maltego.AS',IPtoASN.getASN(val))
+trx.returnOutput()
