ca6aead04739510d575fd782b770be5840f12373
Odin <mgedwar1@gmail.com>
2018-03-22 11:02:54 -0500

    Added basic read-from and add-to source list functionality
    Schema is "link" "source-type"
    
    I'm going to be scraping sources of several types, but preliminarily I'm looking at:
    
    -news
    -social media
       -social media (Twitter)
       -social media (blog)
    
    With OSINT technical IoC streams as well.

--- Gather/Gather.py
+++ Gather/Gather.py
@@ -1,3 +1,18 @@
 import requests
 from bs4 import BeautifulSoup
 
+def getSourceList(filename = './Sources.txt'):
+    f = open(filename, 'r')
+    sourcelist = [[]]
+    for line in f:
+        line_spl = line.split(' ')
+        sourcelist.append([line_spl[1], line_spl[0]])
+    f.close()
+    return sourcelist
+
+def addToSourceList(sourcetype, sourcelink, filename = './Sources.txt'):
+    f = open(filename, 'a')
+
+    f.write(sourcelink+' '+sourcetype+'\n')
+    f.close()
+    return sourcelink+' : '+sourcetype
