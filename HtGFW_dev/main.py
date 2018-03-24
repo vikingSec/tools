076a9524713c4a11fa473373d2e05d2c65242593
vikingSec <mgedwar1@gmail.com>
2018-03-24 18:10:38 -0500

    Fixing silliness

--- HtGFW_dev/main.py
+++ HtGFW_dev/main.py
@@ -1,11 +1,13 @@
+
 import Zhihu
 
 def main(topicFile = 'topics.txt'):
-   
+
     if not Zhihu.ping_url('https://www.zhihu.com'):
         print '[-] NETWORK CONNECTIVITY ISSUES'
         return 1
     print('[+] Connection Available')
-    
+    print(Zhihu.getPost('269692792',True))
+
 
 main()
