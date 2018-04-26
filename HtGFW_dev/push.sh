ad6bc58dc043db5b44cc2ec1a87621fac690acd4
vikingSec <mgedwar1@gmail.com>
2018-04-25 22:08:40 -0500

    Node1 Push

--- HtGFW_dev/push.sh
+++ HtGFW_dev/push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git pull tools_ssh
-git add --all;
-git commit -m "Node1 Push";
+git pull tools_ssh;
+sudo git add --all;
+sudo git commit -m "Node1 Push";
 git push tools_ssh >> ~/GitHub/HtGFW_dev/log_1.txt;
