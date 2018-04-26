31f519ddec9b4490ef4387561f2d6102429e6183
Viking <mgedwar1@gmail.com>
2018-04-25 21:59:19 -0500

    Node1 Push

--- HtGFW_dev/push.sh
+++ HtGFW_dev/push.sh
@@ -2,4 +2,4 @@
 sudo cd ~/GitHub/HtGFW_dev/;
 sudo git add --all;
 sudo git commit -m "Node1 Push";
-sudo git push tools_ssh > ~/GitHub/HtGFW_dev/log_1.txt;
+sudo git push tools_ssh >> ~/GitHub/HtGFW_dev/log_1.txt;
