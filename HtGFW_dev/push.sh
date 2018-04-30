6b005c45ca8d8fb06662a8aeeb9e14463486c80c
vikingSec <mgedwar1@gmail.com>
2018-04-30 22:39:14 +0000

    Help

--- HtGFW_dev/push.sh
+++ HtGFW_dev/push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/node1/GitHub/
-sudo git pull tools_ssh master;
-sudo git add --all;
-sudo git commit -m "Node1 Push";
-sudo git push tools_ssh;
+cd /home/ubuntu/GitHub/
+git pull tools_ssh master;
+git add --all;
+git commit -m "Node1 Push";
+git push tools_ssh;
