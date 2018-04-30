479b3af08b08216a4e29d1b98aa2ccd2c91c80e1
Odin <mgedwar1@gmail.com>
2018-04-30 18:10:25 -0500

    Dying

--- HtGFW_dev/Zhihu.py
+++ HtGFW_dev/Zhihu.py
@@ -4,7 +4,7 @@ import json
 
 
 def get_url(url):
-    r_cap_id
+    
     headers = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
                 'Accept-Encoding': 'gzip, deflate, br',
                 'Accept-Language': 'en-US,en;q=0.5',
